b90dead0b54c473558bc74662dbc5198120fcab0
ruiyaa <ruiyaa>
2025-10-28 11:57:02 +0900

    text arrangement

--- app/page.tsx
+++ app/page.tsx
@@ -2,51 +2,43 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-toge-maru-gothic dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{' '}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{' '}
-            or the{' '}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{' '}
-            center.
-          </p>
+      <main className='flex  flex-col justify-center items-center gap-4 p-4 bg-[#011051]'>
+        <div>
+          <Image src="/img/test.png" alt="Halloween 25 Lit" width={600} height={400} />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={16} height={16} />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <button className="btn btn-primary bg-[#5FFAA1] rounded-full font-toge-maru-gothic py-4  mx-4 w-full">動画を再生</button>
+
+        <div className='flex flex-row w-full gap-4 mx-4 justify-center-safe'>
+          <div>
+            <p>webGLをここに</p>
+            <div className='text-[#F2F3FF] font-toge-maru-gothic'>
+              <h3>操作方法</h3>
+              <p>移動：WASDキー（or 矢印キー）</p>
+              <p>視点：マウス</p>
+            </div>
+          </div>
+          <div className='bg-[#7D56E5] rounded-md flex flex-col items-center-safe'>
+            <h3 className='text-[#F2F3FF] font-toge-maru-gothic '>提供写真</h3>
+            <div className='flex flex-col justify-center'>
+              <Image src="/photo1.jpg" alt="Photo 1" width={200} height={150} />
+              <Image src="/photo2.jpg" alt="Photo 2" width={200} height={150} />
+              <Image src="/photo3.jpg" alt="Photo 3" width={200} height={150} />
+              <Image src="/photo4.jpg" alt="Photo 4" width={200} height={150} />
+              <Image src="/photo5.jpg" alt="Photo 5" width={200} height={150} />
+            </div>
+          </div>
+        </div>
+        <div className='flex flex-col items-start text-[#F2F3FF] font-toge-maru-gothic w-full'>
+          <p>ディレクター：ピナ</p>
+          <p>ゲーム制作：さとたい ぺんぎん アンビ</p>
+          <p>デザイナー：りっちゃん かめさん みるぷ むた</p>
+          <p>WEB制作：あーる</p>
+          <p>バーチャル背景制作：きゅる にとりん</p>
+          <p>映像制作：ゆきま こーた にとりん</p>
+          <p>謎：むた</p>
+          <p>MC：りょうさん</p>
         </div>
       </main>
-    </div>
   );
 }
